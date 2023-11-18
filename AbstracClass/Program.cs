f19847b57988bff95d6f1ce61264aee2d0a80b37
code810 <nadirssh@code.edu.az>
2023-11-19 00:35:30 +0400

    DateTime

--- AbstracClass/Program.cs
+++ AbstracClass/Program.cs
@@ -1,7 +1,17 @@
 ﻿using AbstracClass.Services;
 
-Caculate result=new Caculate();
-Console.WriteLine(result.Divide(9, 3));
-Console.WriteLine(result.Multiple(4,3));
-Console.WriteLine(result.Substract(5, 3));
-Console.WriteLine(result.Plus(5,3));
+//Caculate result=new Caculate();
+//Console.WriteLine(result.Divide(9, 3));
+//Console.WriteLine(result.Multiple(4,3));
+//Console.WriteLine(result.Substract(5, 3));
+//Console.WriteLine(result.Plus(5,3));
+
+//var result = DateTime.Now.ToString("MM/dd/yyyy");
+//var result1 = DateTime.Now.ToString("dddd, dd MMMM yyyy");
+//var result2 = DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm");
+//Console.WriteLine(result);
+//Console.WriteLine(result1);
+//Console.WriteLine(result2);
+
+DateTime Date=DateTime.Now;
+Console.WriteLine(Date.ToString("dd MMMM yyyy"));
